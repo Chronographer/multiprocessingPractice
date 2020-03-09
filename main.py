54cb426f4fb9a14c32261027c3f0109950d51261
Cronographer <djisenberg2015@gmail.com>
2020-03-09 09:58:53 -0400

    Made basic system to experiment with using the python multiprocessing library to allow parallel computation across multiple CPU cores.

---
+++ main.py
@@ -0,0 +1,50 @@
+import multiprocessing
+import numpy as np
+
+sequenceTarget = 3
+printTarget = 100000
+runLinearFlag = False
+runWorkerFlag = True
+
+
+def linearWorker():
+    cycleWasteLinear = []
+    for sequenceI in range(sequenceTarget):
+        for subI in range(0, printTarget):
+            badVar = np.sqrt(np.sin(subI))
+            cycleWasteLinear.append(badVar)
+            #print('Worker: linear  sequence: ' + str(sequenceI) + ' cycle: ' + str(subI))
+        print("sequence: " + str(sequenceI) + " done.")
+    print("linear done.")
+
+
+def worker(workerNumber):
+    """worker function"""
+    cycleWasteList = []
+    for workerI in range(0, printTarget):
+        badVar = np.sqrt(np.sin(workerI))
+        cycleWasteList.append(badVar)
+        #print('Worker: ' + str(workerNumber) + ' cycle: ' + str(workerI))
+    print("worker: " + str(workerNumber) + " done.")
+    return
+
+
+def runWorkers():
+    jobs = []
+    for i in range(sequenceTarget):
+        p = multiprocessing.Process(target=worker, args=(i,))
+        jobs.append(p)
+        p.start()
+
+
+if __name__ == '__main__':
+    if (runWorkerFlag == True):
+        print("starting parallel computation...")
+        jobs = []
+        for i in range(sequenceTarget):
+            p = multiprocessing.Process(target=worker, args=(i,))
+            jobs.append(p)
+            p.start()
+    if(runLinearFlag == True):
+        print("starting single core computation...")
+        linearWorker()
